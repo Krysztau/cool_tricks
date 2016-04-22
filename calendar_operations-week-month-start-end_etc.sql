b7ee43823121c6894bca86e56dca6c9d8691da65
Chris <Krysztau@users.noreply.github.com>
2016-04-23 00:19:24 +0100

    Update and rename calendar_operations-week-month-start-end_etc to calendar_operations-week-month-start-end_etc.sql

--- calendar_operations-week-month-start-end_etc.sql
+++ calendar_operations-week-month-start-end_etc.sql
@@ -1,32 +1,32 @@
 , CONVERT (varchar(10)
        , DATEADD(day, 1 - datepart(weekday, dateadd(day, -1, dateWithSlashes)), dateWithSlashes) 
-       , 103 
+       , 120 
        ) as weekStart
 , CONVERT (varchar(10)
        , DATEADD(day, 7 - datepart(weekday, dateadd(day, -1, dateWithSlashes)), dateWithSlashes) 
-       , 103 
+       , 120 
        ) as weekEnd
 , CONVERT (varchar(10)
        , DATEADD (day, 1 - datepart(day,  dateWithSlashes), dateWithSlashes) 
-       , 103
+       , 120
        ) as Monthstart
 , CONVERT (varchar(10)
        , DATEADD (day, - datepart(day,  DATEADD (month, 1, '2016-01-20 00:00:00.000')) , DATEADD (month, 1, '2016-01-20 00:00:00.000')) 
-       , 103
+       , 120
        ) as Monthend
 , DATEPART(day
 		, DATEADD (day, - datepart(day,  DATEADD (month, 1, '2016-03-30 00:00:00.000')) , DATEADD (month, 1, '2016-03-30 00:00:00.000'))
 		) as MonthLength
 ,  CONVERT (varchar(10)
        , DATEADD (day, 1 - datepart(dayofyear,'2016-01-20 00:00:00.000'),'2016-01-20 00:00:00.000') 
-       , 103
+       , 120
        ) as YearStart
   
 --corrected weeknum (speedtest) - first week = 1st to 7th January and so on...
     , datepart(week,[DATE_LOCAL]) + 
 		(sign(datepart(weekday,[DATE_LOCAL])- datepart(weekday,  DATEADD (day, 1 - datepart(dayofyear,[DATE_LOCAL]),[DATE_LOCAL])+1/2))     -1)/2 as SpecialWeekNum
 
-  FROM [MDW_DWL_PLY].[dbo].[CALENDAR]
+  FROM [DB1].[dbo].[CALENDAR]
 
 --corrected weeknum (maybe slightly slower)
   	, datepart(week,[SESSION_STARTDATE_TIME]) + 
@@ -34,4 +34,4 @@
 		THEN -1
 		ELSE 0
 		END AS SpecWeekNumOPTION2
-	  FROM [MDW_DWL_PLY].[dbo].[CALENDAR]
+	  FROM [DB1].[dbo].[CALENDAR]
